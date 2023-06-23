e326ddccf647229885a5c0dc34841c4c3c01bc50
yongseongjeon <yongseong.jeon95@gmail.com>
2023-06-23 19:19:40 +0900

    feat: add request function

---
+++ src/api/request.ts
@@ -0,0 +1,30 @@
+interface requestProps {
+  url: string;
+  method?: string;
+  body?: object;
+  withAuth?: boolean;
+}
+
+export async function request({ url, method, body }: requestProps): Promise<any> {
+  const headers: HeadersInit = {
+    "Content-Type": "application/json",
+  };
+  const options: RequestInit = { method, headers };
+  const hasBody = !!body;
+  if (hasBody) {
+    options.body = JSON.stringify(body);
+  }
+  return fetch(url, options);
+}
+
+interface fetchHolidaiesProps {
+  year: string;
+  month: string;
+}
+
+export function fetchHolidaies({ year, month }: fetchHolidaiesProps) {
+  const url = `${process.env.REACT_APP_API_URL}/getRestDeInfo`;
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const requestUrl = `${url}?serviceKey=${apiKey}&solYear=${year}&solMonth=${month}&pageNo=1&numOfRows=100`;
+  return request({ url: requestUrl, method: "GET" });
+}

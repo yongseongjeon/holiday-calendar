174ecc1ed1b2956e506426ed93487f15a060933c
yongseongjeon <yongseong.jeon95@gmail.com>
2023-06-23 11:46:38 +0900

    feat: add layout to YearInput page

--- src/pages/YearInput.tsx
+++ src/pages/YearInput.tsx
@@ -1,11 +1,35 @@
+import Text from "../components/Heading/Text";
+import styled from "styled-components";
+import Input from "../components/Input/Input";
+import CreateCalendarButton from "../components/CreateCalendarButton/CreateCalendarButton";
+import useInput from "../hooks/useInput";
+
 function YearInput() {
+  const [year, _, handleChangeInput] = useInput("");
+
   return (
-    <>
-      <h2>공휴일을 확인하고 싶은 연도를 입력해주세요</h2>
-      <input type="number" />
-      <button type="button">캘린더 생성하기</button>
-    </>
+    <Container>
+      <Header>
+        <Text size="1.7rem" weight="700" color="black" value="공휴일을 확인하고 싶은 연도를 입력해주세요"></Text>
+        <Input value={year} onChange={handleChangeInput} />
+      </Header>
+      <CreateCalendarButton />
+    </Container>
   );
 }
 
 export default YearInput;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100vh;
+  padding: 1rem;
+`;
+
+const Header = styled.header`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+`;

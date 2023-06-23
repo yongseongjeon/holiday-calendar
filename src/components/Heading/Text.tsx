7419f28d94a607cdcd20db79d0ea77dec33ad4bb
yongseongjeon <yongseong.jeon95@gmail.com>
2023-06-23 10:41:41 +0900

    feat: create text component

---
+++ src/components/Heading/Text.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+interface SpanProps {
+  size: string;
+  weight: string;
+  color: string;
+}
+
+interface TextProps extends SpanProps {
+  value: string;
+}
+
+export default function Text({ size, weight, color, value }: TextProps) {
+  return (
+    <Span size={size} weight={weight} color={color}>
+      {value}
+    </Span>
+  );
+}
+
+const Span = styled.span<SpanProps>`
+  font-size: ${({ size }) => size}px;
+  font-weight: ${({ weight }) => weight};
+  color: ${({ color }) => color};
+`;

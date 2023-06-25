0dd965be79d7bc87bfbe7cf9974603d847363736
yongseongjeon <yongseong.jeon95@gmail.com>
2023-06-25 20:01:18 +0900

    refactor: optimize calendar drawing from 12 to 1

--- src/hooks/useHoliday.jsx
+++ src/hooks/useHoliday.jsx
@@ -1,40 +1,60 @@
 import { useState, useEffect } from "react";
 import { fetchHolidays } from "../api/request";
 
-const initalStateIsHolidays = Array.from({ length: 13 }, () => Array.from({ length: 32 }, () => false));
+const initialStateIsHolidays = Array.from({ length: 13 }, () => Array.from({ length: 32 }, () => false));
 
 export default function useHoliday(year) {
-  const [isHolidays, setIsHolidays] = useState(initalStateIsHolidays);
+  const [isHolidays, setIsHolidays] = useState(initialStateIsHolidays);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const MONTHS = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
-    MONTHS.forEach(updateIsHolidays);
+    initializeHolidaysThisYear();
 
-    async function updateIsHolidays(month) {
-      const xmlRes = await fetchHolidays({ year, month });
-      const itemElements = await parseXmlResToItemElements(xmlRes);
-      const updatedIsHolidays = markHolidays(itemElements, isHolidays);
+    async function initializeHolidaysThisYear() {
+      const MONTHS = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
+      const promises = MONTHS.map((month) => fetchHolidays({ year, month }));
+      const responses = await Promise.all(promises);
+      const itemElements = await parseXmlResponsesToItemElements(responses);
+      const dateStrings = extractTextFromItemElements(itemElements);
+      const holidays = extractDatesFromDateStrings(dateStrings);
+      const updatedIsHolidays = markHolidays(holidays, isHolidays);
       setIsHolidays(updatedIsHolidays);
+      setIsLoading(false);
     }
   }, []);
-  return isHolidays;
+  return [isHolidays, isLoading];
 }
 
-async function parseXmlResToItemElements(xmlRes) {
-  const resText = await xmlRes.text();
-  const parser = new DOMParser();
-  const xmlDoc = parser.parseFromString(resText, "application/xml");
-  return [...xmlDoc.documentElement.querySelectorAll("item")];
-}
-
-function markHolidays(itemElements, isHolidays) {
+function markHolidays(holidays, isHolidays) {
   const updatedIsHolidays = isHolidays.slice();
-  itemElements.forEach(({ textContent }) => {
-    // e.g. item.textContent = 01어린이날Y202305051
-    const [_, rest] = textContent.split("Y");
-    const month = Number(rest.slice(4, 6));
-    const date = Number(rest.slice(6, 8));
+  holidays.forEach((holiday) => {
+    const [month, date] = holiday;
     updatedIsHolidays[month][date] = true;
   });
   return updatedIsHolidays;
+}
+
+function extractDatesFromDateStrings(dateStrings) {
+  return dateStrings.map((dateString) => {
+    const [_, rest] = dateString.split("Y");
+    const month = Number(rest.slice(4, 6));
+    const date = Number(rest.slice(6, 8));
+    return [month, date];
+  });
+}
+
+function extractTextFromItemElements(itemElements) {
+  return itemElements.map(({ textContent }) => textContent);
+}
+
+async function parseXmlResponsesToItemElements(xmlResponses) {
+  const itemElements = [];
+  for (const response of xmlResponses) {
+    const resText = await response.text();
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(resText, "application/xml");
+    const items = [...xmlDoc.documentElement.querySelectorAll("item")];
+    itemElements.push(...items);
+  }
+  return itemElements;
 }
